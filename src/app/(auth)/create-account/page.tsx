0233a8b510975900f495ea34def223ebaff9f8e1
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-07-23 14:06:13 +0100

    feat: Set up initial login page and routing

---
+++ src/app/(auth)/create-account/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+import React from 'react'
+
+const CreateAccount = () => {
+  return (
+    <>
+      <section>
+          <form action="">
+            
+          </form>
+      </section>
+    </>
+  )
+}
+
+export default CreateAccount
