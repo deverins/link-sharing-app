5af314ea3975ce6c752313ee5049f9c5b67b80e2
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-07-25 00:20:42 +0100

    chore: move Firebase configuration to .env file and update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,16 +1,18 @@
-// firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC4hh9-8f9DhFMyjsnRE43-7GvsJazg4fQ",
-  authDomain: "link-sharing-app-7e7e2.firebaseapp.com",
-  projectId: "link-sharing-app-7e7e2",
-  storageBucket: "link-sharing-app-7e7e2.appspot.com",
-  messagingSenderId: "493197134568",
-  appId: "1:493197134568:web:a8e42e3b2e62e8fc4ed75d"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+
+export { app, auth };
